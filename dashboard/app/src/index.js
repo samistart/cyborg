c730bd1d3e5951148d2c498b75d5e5a563d338f7
samistart <samistart@hotmail.com>
2018-11-18 12:12:06 +0000

    Displaying data from a call to the backend
    This is now a working end-to-end frontend and backend that call each
    other in production. We can now use this infrastructure to add more
    domain-specific functionality.

--- dashboard/app/src/index.js
+++ dashboard/app/src/index.js
@@ -3,21 +3,61 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-class Website extends React.Component {
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
 
-    render() {
+  componentDidMount() {
+    fetch("https://api.lightpay.co/items/")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.items
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
 
-        return (
-            <div className="WebSite">
-                <p>Hello world</p>
-            </div>
-        );
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.name}>
+              {item.name} {item.price}
+            </li>
+          ))}
+        </ul>
+      );
     }
+  }
 }
 
 // ========================================
 
 ReactDOM.render(
-    <Website/>,
+    <Dashboard/>,
     document.getElementById('root')
 );
